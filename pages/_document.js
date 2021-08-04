d2ec5b8da614a2c46d63128f34c4da0096d438f9
bilord <igor.buksheff@yandex.ru>
2021-08-04 18:37:42 +0300

    some fix

--- pages/_document.js
+++ pages/_document.js
@@ -17,6 +17,7 @@ export default class MyDocument extends Document {
                         name="description"
                         content="Всё, что нужно для вашего Образования."
                     />
+                    <link rel="icon" type="image/x-icon" href="/favicon/favicon.ico" />
                     <link rel="apple-touch-icon" sizes="180x180" href="/favicon/apple-touch-icon.png" />
                     <link rel="icon" type="image/png" sizes="32x32" href="/favicon/favicon-32x32.png" />
                     <link rel="icon" type="image/png" sizes="16x16" href="/favicon/favicon-16x16.png" />
